7a8c8636cab41b7ab14597984539c9c2cf6bc085
pbrincker <paulobrincker@hotmail.com>
2022-01-16 19:18:34 -0400

    Adicionado template EJS

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express")
+const path = require('path');
+
 
 const port = 3000;
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res, next)=> {
-    res.send({
+    res.render('index', {
         title: "Meu primeiro Servidor Express",
         version: "0.0.0"
     })
